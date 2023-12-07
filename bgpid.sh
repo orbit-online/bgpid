b5e88d37fef7464fd5ead7ea20d6558f8eac9f90
Anders Ingemann <aim@orbit.online>
2023-12-07 09:19:45 +0100

    Re-add early return when BG_PIDS empty

--- bgpid.sh
+++ bgpid.sh
@@ -22,7 +22,7 @@ bg_run() {
 }
 
 bg_waitany() {
-  [[ $BG_PIDS_OWNER = "$BASHPID" ]] || return 0
+  [[ $BG_PIDS_OWNER = "$BASHPID" && -n $BG_PIDS ]] || return 0
   if ${BG_FAIL:-true}; then
     local pid found=false bg_new_pids ret=0
     while true; do
@@ -51,7 +51,7 @@ bg_waitany() {
 }
 
 bg_block() {
-  [[ $BG_PIDS_OWNER = "$BASHPID" && ${BG_MAXPARALLEL:-4} -gt 0 ]] || return 0
+  [[ $BG_PIDS_OWNER = "$BASHPID" && ${BG_MAXPARALLEL:-4} -gt 0 && -n $BG_PIDS ]] || return 0
   while [[ ${#BG_PIDS[@]} -ge ${BG_MAXPARALLEL:-4} ]]; do
     bg_waitany || return $?
   done
@@ -59,13 +59,13 @@ bg_block() {
 }
 
 bg_killall() {
-  [[ $BG_PIDS_OWNER = "$BASHPID" ]] || return 0
+  [[ $BG_PIDS_OWNER = "$BASHPID" && -n $BG_PIDS ]] || return 0
   kill -"${1:-TERM}" "${BG_PIDS[@]}" 2>/dev/null || true
   return 0
 }
 
 bg_waitall() {
-  [[ $BG_PIDS_OWNER = "$BASHPID" ]] || return 0
+  [[ $BG_PIDS_OWNER = "$BASHPID" && -n $BG_PIDS ]] || return 0
   while [[ ${#BG_PIDS[@]} -gt 0 ]]; do bg_waitany || return $?; done
   return 0
 }
